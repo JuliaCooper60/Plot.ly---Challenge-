2adcf53a48f75d76c6f4341a46e1892fc1af056e
JuliaCooper60 <104178146+JuliaCooper60@users.noreply.github.com>
2022-08-16 11:19:02 +1000

    submit for review

---
+++ app.js
@@ -0,0 +1,10 @@
+d3.json("data/samples.json").then((sampleData) => {
+    var names = sampleData.names
+    console.log(names);
+
+var selecttag - d3.select(#selDataset)
+
+for(var i=0;i<names.length;i++) {
+
+}
+})
